feb32856437be43eb23579f5ad79508c379ad48f
SamLorimer1 <slorimerjr@gmail.com>
2022-04-11 18:35:28 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+# name = input("What is your name?\n")
+# location = input("Where are you from?\n")
+
+def greet_with(name, location):
+    print(f"Hello {name}.")
+    print(f"You are from {location}.")
+
+greet_with("Sam", "Dallas")
+
+
+
